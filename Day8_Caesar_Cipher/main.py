e9c12c5ce99e5934c391d9b056aaae3a017b8a67
nguyent0810 <tunguyen@atomiton.com>
2021-11-02 13:03:50 +0700

    Day8

---
+++ Day8_Caesar_Cipher/main.py
@@ -0,0 +1,30 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+        end_text += char
+    else:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+print(art.logo)
+
+continue_program = "yes"
+while continue_program == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if shift > 26:
+        shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    continue_program = (input("Do you want to continue? Input 'Yes' to restart, 'No' for stop the program\n")).lower()
