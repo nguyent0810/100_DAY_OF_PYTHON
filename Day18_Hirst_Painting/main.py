f21ceaa169656a5e7668236add9033a0c9ab8cb9
nguyent0810 <tunguyen@atomiton.com>
2021-11-12 16:29:16 +0700

    Day18

---
+++ Day18_Hirst_Painting/main.py
@@ -0,0 +1,34 @@
+# import colorgram
+import turtle as turtle_module
+import random
+# colors = colorgram.extract("image.jpg", 42)
+# rgb_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     rgb_colors.append(new_color)
+
+color_list = [(26, 108, 164), (193, 38, 81), (237, 161, 50), (234, 215, 86), (227, 237, 229), (223, 137, 176), (143, 108, 57), (103, 197, 219), (21, 57, 132), (205, 166, 30), (213, 74, 91), (238, 89, 49), (142, 208, 227), (119, 191, 139), (5, 185, 177), (106, 108, 198), (137, 29, 72), (4, 162, 86), (98, 51, 36), (24, 155, 210), (229, 168, 185), (173, 185, 221), (29, 90, 95), (233, 173, 162), (156, 212, 190), (87, 46, 33), (37, 45, 83), (245, 205, 7), (35, 88, 88), (103, 24, 56)]
+
+tim = turtle_module.Turtle()
+tim.penup()
+tim.hideturtle()
+screen = turtle_module.Screen()
+screen.colormode(255)
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dot = 100
+
+for dot_count in range(1, number_of_dot + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
