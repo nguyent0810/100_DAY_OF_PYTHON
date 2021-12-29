54b93d1f1cd5f3871effb54f567f96d057f4e3f5
nguyent0810 <tung.hdv@gmail.com>
2021-12-29 18:15:52 +0700

    Day57

--- Day57_Templating_With_Jinja/main.py
+++ Day57_Templating_With_Jinja/main.py
@@ -1,31 +1,31 @@
 from flask import Flask, render_template
-import random
-import datetime
+from post import Post
 import requests
+
+posts = requests.get("https://api.npoint.io/5da281d06062f7a0f414").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    random_number = random.randint(1, 10)
-    current_year = datetime.datetime.now().year
-    my_name = "Tung Nguyen"
-    return render_template("index.html", num=random_number, current_year=current_year, my_name=my_name)
-@app.route('/guess/<name>')
-def guess(name):
-    gender_response = requests.get(url=f"https://api.genderize.io?name={name}")
-    data = gender_response.json()
-    gender = data['gender']
-    age_response = requests.get(url=f'https://api.agify.io?name={name}')
-    age_data = age_response.json()
-    age = age_data['age']
-    return render_template('guess.html', name=name, gender=gender, age=age)
 
-@app.route('/blog/<blog_id>')
-def get_blog(blog_id):
-    blog_url = 'https://api.npoint.io/5da281d06062f7a0f414'
-    response = requests.get(url=blog_url)
-    all_posts = response.json()
-    return render_template('blog.html', posts=all_posts)
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
